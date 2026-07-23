eff5b3d1f9330c3582c425c66cb959144f0d12b6
CI User <ci@example.com>
2026-07-23 21:59:11 +0300

    fix: v2.3.0 major release - automatic channel cache purge at startup, v20 channels with fullScreenIntent and hardware alarm vibration pattern

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -82,7 +82,7 @@ class AboutScreen extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20),
               ),
               child: Text(
-                '${settingsProvider.tr('version')} 2.2.9',
+                '${settingsProvider.tr('version')} 2.3.0',
                 style: TextStyle(
                   color: isDark ? Colors.white : primaryColor,
                   fontWeight: FontWeight.bold,
